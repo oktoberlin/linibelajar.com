5db307daa372f6637dbbe88334d3d447575a5a81
Acer <oktoberlin@gmail.com>
2020-07-13 02:09:55 +0700

    change4

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), '/django_project/static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
