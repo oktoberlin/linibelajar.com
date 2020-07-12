87d20b8aacbd98adcb645190c26d3c1ef8c357ac
Acer <oktoberlin@gmail.com>
2020-07-13 02:19:22 +0700

    add media url

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,8 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(
-    BASE_DIR), '/django_project/static/')
+MEDIA_URL = '/image/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
