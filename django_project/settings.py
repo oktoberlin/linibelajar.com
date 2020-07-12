86cb7e2089cd65de40aa2426bab77eff0c98bfed
Acer <oktoberlin@gmail.com>
2020-07-13 01:57:16 +0700

    modify static root

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/django/django_project/django_project/static/'
 
-# STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
